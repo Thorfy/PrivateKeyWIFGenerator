eb59e8fb736d751cb7d632d2d9c68447556c3cfb
lefebvreRemy <remy.lefebvre@meteoconsult.com>
2019-09-02 10:18:58 +0200

    auto indent

--- Create.js
+++ Create.js
@@ -19,26 +19,26 @@ console.log('> Blockchain info: ',uri);
 
 function createPublicHash(privateKey){
 
-let keys = ecdsa.keyFromPrivate(privateKey);  
-let publicKey = keys.getPublic('hex');  
+	let keys = ecdsa.keyFromPrivate(privateKey);  
+	let publicKey = keys.getPublic('hex');  
 
-let hash = sha256(Buffer.from(publicKey, 'hex'));
-let publicKeyHash = new ripemd160().update(Buffer.from(hash, 'hex')).digest();
-console.log('> Public hash created: ', publicKeyHash.toString("hex"));
+	let hash = sha256(Buffer.from(publicKey, 'hex'));
+	let publicKeyHash = new ripemd160().update(Buffer.from(hash, 'hex')).digest();
+	console.log('> Public hash created: ', publicKeyHash.toString("hex"));
 
 return publicKeyHash.toString("hex");
 
 }
 function createPublicAddress(publicKeyHash) {
-  const step1 = Buffer.from("00" + publicKeyHash, 'hex');
-  const step2 = sha256(step1);
-  const step3 = sha256(Buffer.from(step2, 'hex'));
-  const checksum = step3.substring(0, 8);
-  const step4 = step1.toString('hex') + checksum;
-  const address = base58.encode(Buffer.from(step4, 'hex'));
+ 	const step1 = Buffer.from("00" + publicKeyHash, 'hex');
+  	const step2 = sha256(step1);
+  	const step3 = sha256(Buffer.from(step2, 'hex'));
+  	const checksum = step3.substring(0, 8);
+  	const step4 = step1.toString('hex') + checksum;
+  	const address = base58.encode(Buffer.from(step4, 'hex'));
 
-  console.log('> Public key created: ',address)
-  return address;
+  	console.log('> Public key created: ',address)
+  	return address;
 }
 
 function createPrivateKeyWIF(privateKey) {
@@ -47,11 +47,11 @@ function createPrivateKeyWIF(privateKey) {
     const step2 = sha256(step1);
     const step3 = sha256(Buffer.from(step2, 'hex'));
     const checksum = step3.substring(0, 8);
-  const step4 = step1.toString('hex') + checksum;
+  	const step4 = step1.toString('hex') + checksum;
     const privateKeyWIF = base58.encode(Buffer.from(step4,"hex"));
 
   
-  console.log('> Private key: '+ privateKey.toString('hex'));
+  	console.log('> Private key: '+ privateKey.toString('hex'));
     console.log('> privateKeyWIF: '+ privateKeyWIF);
     
     return privateKeyWIF;
